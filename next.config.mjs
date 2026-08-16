8a13c830040f12e662d0e2d635bb76b7b7d0afd3
Akash Gamage <maneeshaakash2005@gmail.com>
2026-08-16 21:04:33 +0530

    Fix : Update nextConfig file for the build issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,11 @@
-  import { ImageError } from 'next/dist/server/image-optimizer';
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: "export",
 
-  /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    reactStrictMode: true,
-    output: "export",
-    images: {
-      unoptimized: true,
-    },
-  };
+  images: {
+    unoptimized: true,
+  },
+};
 
-  export default nextConfig;
+export default nextConfig;

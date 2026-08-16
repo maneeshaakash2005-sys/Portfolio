0c4a2b84d475ba609a265b8bc00ad850e18a30f0
Akash Gamage <maneeshaakash2005@gmail.com>
2026-08-16 20:56:00 +0530

    Fix : Update next-config image formet

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
+import { ImageError } from 'next/dist/server/image-optimizer';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
   images: {
-    formats: ["image/avif", "image/webp"],
+    formats: ["image/avif", "image/webp","image/png", "image/jpg"],
   },
 };
 
